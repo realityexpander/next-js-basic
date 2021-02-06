247a9e8471e3348219a4d29e801c6f248c7b4776
Chris Athanas <chris@crowdskout.com>
2021-02-06 15:49:31 -0600

    add paths, props

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -1,8 +1,46 @@
 import { useRouter } from 'next/router'
+import Head from 'next/head'  // <-- for SEO
 
-export default function Car() {
+export default function Car({ car }) {  // <-- 'car' prop received from getStaticProps
   const router = useRouter()
   const { id } = router.query
 
-  return <h1>Hello {id}</h1>
+  return ( 
+    <>
+      <Head>
+        <title>{car.color} {car.id} </title>
+      </Head>
+
+      <h1>Hello {id}</h1>
+      <img src={car.image} width="300px"/>
+    </>
+  )
+}
+
+export async function getStaticProps({ params }) {  // <-- id of car from the Query params [id], ie: "ford"
+  console.log({params})
+  
+  const req = await fetch(`http://localhost:3000/${params.id}.json`)
+  const data = await req.json()
+
+  console.log({data})
+
+  return {
+    props: { car: data },  // <-- 'car' data prop returned here
+  }
+}
+
+export async function getStaticPaths() {  // <-- gets all the routes
+
+    const req = await fetch('http://localhost:3000/cars.json');
+    const data = await req.json();
+
+    const paths = data.map(car => {
+        return { params: { id: car } }
+    })
+
+    return {
+        paths,
+        fallback: false
+    };
 }
